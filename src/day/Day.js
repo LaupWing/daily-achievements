6b5fa637554002b16aa19f6464e78f21ed75516d
LaupWing <loc_strict@hotmail.com>
2019-11-19 17:25:12 +0100

    added color indicator

--- src/day/Day.js
+++ src/day/Day.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import {connect} from 'react-redux'
 const Day = ({day, skills}) => {
-    console.log(day,skills)
+    
     const skillColors = skills.filter(skill=>{
         return day.learned.some(l=>l.skill === skill.title)
     })
-    console.log(skillColors)
+    const calcWidth = ()=>{
+       return 100/skillColors.length +'%' 
+    }
+    const setStyle = (sc)=>{
+        return{
+            background: sc.color,
+            width: calcWidth()
+        }
+    }
     return (
         <div className="day"> 
             <div className="skill-indicator">
-
+                {skillColors && skillColors.map(sc=>{
+                    return(
+                        <div className="skill-color" style={setStyle(sc)}></div>
+                    )
+                })}
             </div>
         </div>
     )
