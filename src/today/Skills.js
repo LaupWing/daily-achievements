fd8b15786c266cff2c27634214cba35c50137aa1
LaupWing <loc_strict@hotmail.com>
2019-11-19 21:42:06 +0100

    added the skill container

---
+++ src/today/Skills.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Skills() {
+    return (
+        <div>
+            <h3>Today i have learned something from the skill:</h3>
+        </div>
+    )
+}
+
+export default Skills
--- src/today/Today.js
+++ src/today/Today.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
 class Today extends Component {
+    dateInString(){
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+        ];
+        const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        const date = new Date()
+        return `${days[date.getDay()]}, ${date.getDate()} ${monthNames[date.getMonth()]}`
+    }
     render() {
         return (
             <div className="Today">
-                'Today'
+                <h2>{this.dateInString()}</h2>
             </div>
         )
     }
