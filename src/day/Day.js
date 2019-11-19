27724efcfb19c1b9c0ca8112e53bfd29830373f4
LaupWing <loc_strict@hotmail.com>
2019-11-19 17:06:56 +0100

    added skillColors var in day.js

--- src/day/Day.js
+++ src/day/Day.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import {connect} from 'react-redux'
 const Day = ({day, skills}) => {
-
+    console.log(day,skills)
+    const skillColors = skills.filter(skill=>{
+        return day.learned.some(l=>l.skill === skill.title)
+    })
+    console.log(skillColors)
     return (
         <div className="day"> 
-            
+            <div className="skill-indicator">
+
+            </div>
         </div>
     )
 }
