9531980dbe460950e9a2305d0fe09bef418be517
LaupWing <loc_strict@hotmail.com>
2019-11-20 14:49:35 +0100

    added dynamic classes to the skills

--- src/today/Skills.js
+++ src/today/Skills.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 export class Skills extends Component {
+    state={
+        skillsToday:['Photoshop'],
+        skillActive: null
+    }
+    additionalClasses=({title})=>{
+        const skillIncludes = this.state.skillsToday.includes(title) 
+        let additionalClass = null
+        if(skillIncludes){
+            additionalClass = 'doneToday'
+        }
+        if(this.state.skillActive === title){
+            additionalClass = additionalClass + ' active'
+        }
+        return additionalClass ? additionalClass : ''
+    }
+    handleClick=({title})=>{
+        this.setState({
+            skillActive: title
+        },()=>{
+            console.log(this.state)
+        })
+        
+    }
     render() {
         const {skills} = this.props
         return (
@@ -9,7 +32,7 @@ export class Skills extends Component {
                 <div className="skills-container">
                     {skills && skills.map(skill=>{
                         return(
-                            <div className="skill">
+                            <div onClick={()=>this.handleClick(skill)} className={"skill "+this.additionalClasses(skill)}>
                                 <h3 style={{background:skill.color}}>{skill.title.slice(0,2)}</h3>
                                 <p>{skill.title}</p>
                             </div>
