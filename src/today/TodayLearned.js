76ee7df11aa0cb7a6383733c18916b0cf9cb6837
LaupWing <loc_strict@hotmail.com>
2019-11-20 19:58:59 +0100

    added the today learned p

--- src/today/TodayLearned.js
+++ src/today/TodayLearned.js
@@ -1,22 +1,34 @@
 import React from 'react'
 
 function TodayLearned({today, skill}) {
-    console.log(today, skill)
+    // console.log(today, skill)
     const onlyUnique = (value, index, self)=> { 
         return self.indexOf(value) === index;
     }
     const skillToday = today.learned
         .find(l=>l.skill === skill.title)
+
     const categories = skillToday.whatHaveILearned
         .map(whi=>whi.category)
         .filter(onlyUnique)
-    console.log(skillToday)
+        
+    const corresponding = (c)=>{
+        const categoryLearned = skillToday.whatHaveILearned.filter(c2=>c2.category === c)
+        const pEl =  categoryLearned.map(cl=>{
+                return(
+                    <p style={{borderColor:skill.color}}>{cl.learned}</p>
+                )
+            })
+        return pEl
+    }
+
     return (
         <div className='today-learned'>
             {categories && categories.map(c=>{
                 return(
                     <div className="newly-learned">
                         <h4 style={{background:skill.color}}>{c}</h4>
+                        {corresponding(c)}
                     </div>
                 )
             })}
