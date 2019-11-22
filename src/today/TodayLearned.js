5d265f43740f7cf6318be45520b0314accf46fd2
LaupWing <loc_strict@hotmail.com>
2019-11-22 01:32:31 +0100

    succesfull changed the todaylearned file to class component

--- src/today/TodayLearned.js
+++ src/today/TodayLearned.js
@@ -1,50 +1,59 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function TodayLearned({today, skill}) {
-    // console.log(today, skill)
-    const onlyUnique = (value, index, self)=> { 
+class TodayLearned extends Component{
+    onlyUnique = (value, index, self)=> { 
         return self.indexOf(value) === index;
     }
-    console.log(today,skill)
-    const skillToday = today.learned
-        .find(l=>l.skill === skill.title)
-
-    const categories = skillToday.whatHaveILearned
-        .map(whi=>whi.category)
-        .filter(onlyUnique)
-        
-    const corresponding = (c)=>{
-        const categoryLearned = skillToday.whatHaveILearned.filter(c2=>c2.category === c)
+    
+    corresponding = (c)=>{
+        const {skill} = this.props 
+        const categoryLearned = this.skillToday().whatHaveILearned.filter(c2=>c2.category === c)
         const pEl =  categoryLearned.map(cl=>{
-                return(
-                    <p style={{borderColor:skill.color}}>{cl.learned}</p>
+            return(
+                <p style={{borderColor:skill.color}}>{cl.learned}</p>
                 )
             })
-        return pEl
+            return pEl
+        }
+    skillToday = ()=>{
+        const {today} = this.props 
+        const {skill} = this.props
+        return today.learned
+           .find(l=>l.skill === skill.title)
     }
 
-    return (
-        <div className='Today-learned'>
-            <h4>Categories and what i have learned today </h4>
-            <div className="buttons">
-                <button>Add New +</button>
-                <div className="extra">
-                    <button>Today</button>
-                    <button className='active'>All</button>
+    categories = ()=>{
+        return this.skillToday().whatHaveILearned
+            .map(whi=>whi.category)
+            .filter(this.onlyUnique)
+    } 
+    render(){
+        const {skill} = this.props
+        return (
+            <div className='Today-learned'>
+                <h4>Categories and what i have learned today </h4>
+                <div className="buttons">
+                    <button>Add New +</button>
+                    <div className="extra">
+                        <button>Today</button>
+                        <button className='active'>All</button>
+                    </div>
+                </div>
+                <div className="newly-learned-container">
+                    {this.categories() && this.categories().map(c=>{
+                        return(
+                            <div className="newly-learned">
+                                <h5 style={{background:skill.color}}>{c}</h5>
+                                <div className="learned-wrapper">
+                                    {this.corresponding(c)}
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
-            <div className="newly-learned-container">
-                {categories && categories.map(c=>{
-                    return(
-                        <div className="newly-learned">
-                            <h5 style={{background:skill.color}}>{c}</h5>
-                            {corresponding(c)}
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default TodayLearned
